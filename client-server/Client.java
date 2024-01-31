a95820c3af39dbf2cf7227b0b2079a07205b1d94
Jhedie <jeddiahawuku12@gmail.com>
2024-01-31 20:59:01 +0000

    feat: Enable client-server communication
    Add key pairs for server, alice and bob

--- client-server/Client.java
+++ client-server/Client.java
@@ -4,25 +4,40 @@ import java.net.*;
 class Client {
 
     public static void main(String[] args) throws Exception {
-
+        
         if (args.length < 3) {
             System.err.println("Usage: java Client <host> <port> <userid>");
             System.exit(1);
         }
-
         String host = args[0]; // hostname of server
         int port = Integer.parseInt(args[1]); // port of server
         String userid = args[2]; // user id
 
         try {
+            String serverString, clientString;
+
             Socket s = new Socket(host, port);
-            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            DataInputStream din = new DataInputStream(s.getInputStream());
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-            dos.writeUTF("Hello World! From user: " + userid);
-            dos.writeUTF("Happy new year! From user: " + userid);
+            do {
+                System.out.println("Enter a message for the client> "); // prompt the user
+                clientString = br.readLine(); // get user input
+                dout.writeUTF(clientString); // add to an output stream
+                dout.flush(); // send message
 
+                serverString = din.readUTF();
+                if (serverString.equals("stop")) {
+                    break;
+                }
+                System.out.println("Server says: " + serverString);
+
+            } while (!serverString.equals("stop"));
+            dout.close();
+            s.close();
         } catch (Exception e) {
-            System.err.println("Cannot connect to server.");
+            System.out.println(e);
         }
 
     }
--- client-server/Server.java
+++ client-server/Server.java
@@ -4,27 +4,40 @@ import java.net.*;
 class Server {
 
     public static void main(String[] args) throws Exception {
-
-        if (args.length < 1) {
-            System.err.println("Usage: java Server <port>");
-            System.exit(1);
-        }
-
-        int port = Integer.parseInt(args[0]); // port of server
-        ServerSocket ss = new ServerSocket(port);
-        System.out.println("Waiting incoming connection...");
-
-        Socket s = ss.accept();
-        DataInputStream dis = new DataInputStream(s.getInputStream());
-
-        String x = null;
-
         try {
-            while ((x = dis.readUTF()) != null) {
+            String serverString, clientString;
 
-                System.out.println(x);
-
+            if (args.length < 1) {
+                System.err.println("Usage: java Server <port>");
+                System.exit(1);
             }
+
+            int port = Integer.parseInt(args[0]); // port of server
+
+            System.out.println("Waiting incoming connection...");
+            ServerSocket ss = new ServerSocket(port);
+            Socket s = ss.accept();
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            do {
+                clientString = dis.readUTF(); // read input for the client
+                System.out.println("Client says " + clientString);
+                if (clientString.equals("stop")) {
+                    break;
+                }
+                System.out.print("Write something to client > ");// prom
+                serverString = br.readLine(); // read their input
+
+                dout.writeUTF(serverString);// write it into the output stream.
+                dout.flush();// send
+
+            } while (!serverString.equals("stop"));// stop if stop
+
+            dis.close();
+            s.close();
+            ss.close();
         } catch (IOException e) {
             System.err.println("Client closed its connection.");
         }
