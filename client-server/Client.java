fc693b89ac0dc291ef83e1db2b5181615a8f2679
Jhedie <jeddiahawuku12@gmail.com>
2024-02-02 10:20:42 +0000

    feat: Encrypt message, add signature, timestamp and send to the server

--- client-server/Client.java
+++ client-server/Client.java
@@ -1,15 +1,35 @@
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.time.Instant;
+import java.util.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 class Client {
 
-    public static void main(String[] args) throws Exception {
+    private static String userid = "";
+
+    public static void main(String[] args) {
 
         if (args.length < 3) {
             System.err.println("Usage: java Client <host> <port> <userid>");
@@ -17,7 +37,7 @@ class Client {
         }
         String host = args[0]; // hostname of server
         int port = Integer.parseInt(args[1]); // port of server
-        String userid = args[2]; // user id
+        userid = args[2]; // user id
 
         try (Socket s = new Socket(host, port);
                 DataInputStream dis = new DataInputStream(s.getInputStream());
@@ -34,6 +54,14 @@ class Client {
                     while ((msg = dis.readUTF()) != null) {
                         System.out.println(msg);
 
+                        // Upon receiving these contents,
+                        // the client should first verify the signature with the appropriate key.
+                        // If the key does not verify, it should terminate the connection immediately.
+                        // Otherwise, it decrypts the message with the appropriate key, and displays the
+                        // decrypted message and the timestamp on screen.
+
+                        // After displaying all these messages, the client program then asks the user
+                        // whether they want to send a message.
                         System.out.println("Do you want to send a message? (y/n)");
                         String userInput = br.readLine();
                         if ("y".equalsIgnoreCase(userInput)) {
@@ -43,7 +71,24 @@ class Client {
                             String message = br.readLine();
 
                             String messageToSend = recipient + "|" + message;
-                            dos.writeUTF(messageToSend);
+                            // encrypt message
+                            byte[] encryptedMessage = encryptMessage(messageToSend);
+
+                            // Generate a timestamp
+                            Instant timestamp = getTimestamp();
+
+                            // generate a signature
+                            byte[] digitalSignature = getDigitalSignature(encryptedMessage);
+
+                            // Send the encrypted message, timestamp, signature, and unhashed sender userid
+                            // to the server
+                            dos.writeUTF(userid);
+                            dos.writeUTF(Base64.getEncoder().encodeToString(digitalSignature));
+                            dos.writeUTF(timestamp.toString());
+                            dos.writeUTF(Base64.getEncoder().encodeToString(encryptedMessage));
+
+                            System.out.println("Message sent!");
+                            dos.flush();
                             break;
                         } else {
                             System.out.println("Goodbye!");
@@ -51,18 +96,34 @@ class Client {
                         }
 
                     }
-                } catch (IOException e) {
-                    System.err.println("Server closed its connection.");
+                } catch (IOException | InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException
+                        | IllegalBlockSizeException | BadPaddingException | InvalidKeySpecException
+                        | SignatureException e) {
+                    System.err.println("Error: " + e.getMessage());
                 }
             });
             readingThread.start();
 
             // Wait for the reading thread to finish
             readingThread.join();
-        } catch (IOException | NoSuchAlgorithmException e) {
+        } catch (IOException | NoSuchAlgorithmException | InterruptedException e) {
             System.out.println("Error: " + e.getMessage());
         }
 
+    }
+
+    private static Instant getTimestamp() {
+        Instant timestamp = Instant.now();
+        return timestamp;
+    }
+
+    private static byte[] getDigitalSignature(byte[] encryptedMessage) throws NoSuchAlgorithmException,
+            InvalidKeyException, IOException, InvalidKeySpecException, SignatureException {
+        Signature signature = Signature.getInstance("SHA256withRSA");
+        signature.initSign(getClientPrivateKey());
+        signature.update(encryptedMessage);
+        byte[] digitalSignature = signature.sign();
+        return digitalSignature;
     }
 
     private static String hashUserId(String userid) throws NoSuchAlgorithmException {
@@ -73,9 +134,47 @@ class Client {
         for (byte b : digest) {
             sb.append(String.format("%02X", b));
         }
-        String s = sb.toString();
+        String hashedString = sb.toString();
 
-        return s;
+        return hashedString;
+
+    }
+
+    private static PublicKey getServerPublicKey()
+            throws IOException, InvalidKeySpecException, NoSuchAlgorithmException {
+        File f = new File("./keypairs/server.pub");
+        byte[] keyBytes = Files.readAllBytes(f.toPath());
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory;
+        keyFactory = KeyFactory.getInstance("RSA");
+
+        PublicKey publicKey = keyFactory.generatePublic(spec);
+
+        return publicKey;
+    }
+
+    private static PrivateKey getClientPrivateKey()
+            throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+
+        File f = new File("./keypairs/" + userid + ".prv");
+        byte[] keyBytes = Files.readAllBytes(f.toPath());
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory;
+        keyFactory = KeyFactory.getInstance("RSA");
+
+        PrivateKey privateKey = keyFactory.generatePrivate(spec);
+
+        return privateKey;
+    }
+
+    private static byte[] encryptMessage(String message)
+            throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException,
+            BadPaddingException, InvalidKeySpecException, IOException {
+        PublicKey serverPublicKey = getServerPublicKey();
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, serverPublicKey);
+        byte[] encryptedMessage = cipher.doFinal(message.getBytes());
+        return encryptedMessage;
 
     }
 
--- client-server/Server.java
+++ client-server/Server.java
@@ -9,6 +9,8 @@ import java.util.HashMap;
 class Server {
     private static final HashMap<String, ArrayList<String>> messages = new HashMap<>();
 
+    private static final String currentUser = "";
+
     public static void main(String[] args) throws Exception {
 
         if (args.length < 1) {
@@ -39,11 +41,43 @@ class Server {
 
             System.out.println("Delivering " + messages.get(clientMessage).size() + " messages");
             dos.writeUTF("There are " + messages.get(clientMessage).size() + " message(s) for you");
+            if (messages.get(clientMessage).size() > 0) {
+                // Send each message to the client
+                for (String msg : messages.get(clientMessage)) {
+                    dos.writeUTF(msg);
+
+                    // Upon receiving these contents, the server first verifies the signature with
+                    // the appropriate key.
+
+                    // If the signature does not verify, or if the sender userid is unrecognised (no
+                    // corresponding key of that userid is present in the server) the message is
+                    // discarded
+
+                    // Otherwise, it decrypts the message, and finds out the recipient userid.
+
+                    // If the decryption fails (i.e., it results in a BadPaddingException), the
+                    // message is again discarded.
+
+                    // The server then re-encrypts the message (but without the recipient userid).
+
+                    // Finally the server computes the hashed recipient userid, and saves it and the
+                    // encrypted message to its collection of messages. The original (unhashed)
+                    // recipient userid is not stored. The signature is also not stored.
+
+                    // The connection then ends and the server should wait for the next client. The
+                    // server should not quit or terminate (even if the signature check fails or the
+                    // client terminated their connection early).
+
+                }
+
+                // Clear the messages for this user
+                messages.get(clientMessage).clear();
+            }
 
             String msg;
             while ((msg = dis.readUTF()) != null) {
                 System.out.println(msg);
-                dos.writeUTF(msg);
+
             }
         } catch (IOException e) {
             System.err.println("Client closed its connection.");
